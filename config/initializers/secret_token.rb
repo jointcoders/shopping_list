34d84c2a16b45f7f55bffbaebe37deda7fcb1ad5
root <root@debian.hsd1.ca.comcast.net>
2013-01-21 03:16:57 -0800

    Formed the skeletal base of a rails project to begin moving it to be the new app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopping::Application.config.secret_token = 'b1f86f9c68bcea8bc8dd9202739e3486ebbb6f005b9466cd9d4533bdd94d389bb3f854448ca1bae15de9160133e97ff51e0b3c024eae4d29d255c8b3774bca43'
